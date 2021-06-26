58ca5fc3090e6859a10a906b10ae64d068f5c2ac
Eule Hoelz Colla <ecolla@netpoint.com.br>
2021-06-26 12:29:23 -0300

    Service layer, component registration

---
+++ src/main/java/com/eulecorp/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.eulecorp.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eulecorp.course.entities.User;
+import com.eulecorp.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id){
+
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
