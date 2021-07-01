dd8cc36c4f7259037fac4c4bd7345c7f191270c2
Eule Hoelz Colla <ecolla@netpoint.com.br>
2021-06-30 22:32:39 -0300

    Exception handling - findById

--- src/main/java/com/eulecorp/course/services/UserService.java
+++ src/main/java/com/eulecorp/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.eulecorp.course.entities.User;
 import com.eulecorp.course.repositories.UserRepository;
+import com.eulecorp.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,8 @@ public class UserService {
 	public User findById(Long id){
 
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+	//	return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
